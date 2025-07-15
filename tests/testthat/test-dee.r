7433b491a5f845f017cd33ef6648e9811edf41e7
Trevor L. Davis <trevor.l.davis@gmail.com>
2025-07-14 20:32:52 -0700

    feat: Allow "dee" class to be longer than 1
    * `dee()` now allows class to be longer than 1
    * New `c.dee()` method
    * Revise `print.dee()` method

--- tests/testthat/test-dee.r
+++ tests/testthat/test-dee.r
@@ -4,5 +4,7 @@ test_that("`dee()`", {
     expect_true(inherits(format(d), "character"))
     s <- capture.output({d2 <- print(d)})
     expect_equal(d, d2)
-    expect_equal(s, "M 10,30 V 30")
+    expect_equal(s, c("<dee[1]>", "M 10,30 V 30"))
+
+    expect_true(inherits(c(d, d2), "dee"))
 })
