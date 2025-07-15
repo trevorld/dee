36bd04f35cc01c88a3aac5088cc5860dcd577392
Trevor L Davis <trevor.l.davis@gmail.com>
2025-07-15 11:53:04 -0700

    feat: `dee_options()`
    * `dee_options()` returns a list of (current or default) `{dee}` package option values.
    * Collect documentation on these options in new `dee-package` man page.

---
+++ R/dee-package.r
@@ -0,0 +1,15 @@
+#' @section Package options:
+#'  The following `dee` option may be set globally via [base::options()]:
+#'   \describe{
+#'     \item{dee.attrs}{[plot.dee()] passes this to [omsvg::svg_path()]'s `attrs` argument.}
+#'     \item{dee.height}{Passed to `height` arguments; the height of svg image (in pixels).}
+#'     \item{dee.width}{Passed to `width` arguments; the width of svg image (in pixels).}
+#'     \item{dee.background_color}{Passed to [plot.dee()]'s `background_color` argument.}
+#'     \item{dee.fill}{[plot.dee()] passes this to [omsvg::svg_path()]'s `fill` argument.}
+#'     \item{dee.stroke}{[plot.dee()] passes this to [omsvg::svg_path()]'s `stroke` argument.}
+#'     \item{dee.stroke_width}{[plot.dee()] passes this to [omsvg::svg_path()]'s `stroke_width` argument.}
+#'     \item{dee.origin_at_bottom}{Passed to `origin_at_bottom` argument of svg d path command functions.}
+#'     \item{dee.sep}{Passed to `sep` argument of svg d path command functions.}
+#'   }
+#' @keywords internal
+"_PACKAGE"
---
+++ R/dee_options.r
@@ -0,0 +1,46 @@
+default_options <- list(dee.sep = ",",
+                        dee.origin_at_bottom = FALSE,
+                        dee.height = NULL)
+
+#' Get dee options
+#'
+#' `dee_options()` returns the `dee` package's global options.
+#'
+#' @param ... `dee` package options using `name = value`.
+#'            The return list will use any of these instead of the current/default values.
+#' @param default If `TRUE` return the default values instead of current values.
+#' @return A list of option values.
+#'         Note this function **does not** set option values itself but
+#'         this list can be passed to [options()], [withr::local_options()], or [withr::with_options()].
+#' @examples
+#'   dee_options()
+#'
+#'   dee_options(default = TRUE)
+#'
+#'   dee_options(dee.height = 100, dee.width = 100, dee.origin_at_bottom = TRUE)
+#' @seealso [dee-package] for a high-level description of relevant global options.
+#' @export
+dee_options <- function (..., default = FALSE) {
+    dee_op <- list(dee.attrs = NULL,
+                   dee.height = NULL,
+                   dee.width = NULL,
+                   dee.background_color = NULL,
+                   dee.fill = NULL,
+                   dee.stroke = NULL,
+                   dee.stroke_width = NULL,
+                   dee.origin_at_bottom = FALSE,
+                   dee.sep = ",")
+    l <- list(...)
+    stopifnot(all(names(l) %in% names(dee_op)))
+    if (isFALSE(default)) {
+        for (n in names(dee_op)) {
+            dee_op[n] <- list(getOption(n, dee_op[[n]]))
+        }
+    }
+    if (length(names(l))) {
+        for (n in names(l)) {
+            dee_op[n] <- l[n]
+        }
+    }
+    dee_op
+}
