e88870c90152b15a1d7a5ae5593b32c56568f97e
Trevor L. Davis <trevor.l.davis@gmail.com>
2025-07-23 22:05:23 -0700

    feat: `ELLIPSE()` and `POLYGON()`
    * `ELLIPSE()` is a wrapper around `M()` and `AZ()` to build ellipse paths.
    * `POLYGON()` is a wrapper around `MZ()` to build polygon paths.
      If its argument `offset` is nonzero will compute an offset region.

--- R/path_wrappers.r
+++ R/path_wrappers.r
@@ -1,6 +1,6 @@
 #' Rectangle path convenience wrapper
 #'
-#' `MZ_RECT()` is a wrapper around `MZ()` to create
+#' `RECT()` is a wrapper around `MZ()` to create
 #' rectangle shaped paths.
 #' @inheritParams M
 #' @param w,h The width and height of the rectangle.
@@ -8,6 +8,12 @@
 #' @return A [dee()] object.
 #' @examples
 #' RECT(x = 5, y = 5, w = 4, h = 6)
+#' if (requireNamespace("omsvg", quietly = TRUE) &&
+#'     requireNamespace("svgparser", quietly = TRUE)) {
+#'   plot(RECT(x = 5, y = 5, w = 4, h = 6),
+#'        height = 10, width = 10,
+#'        fill = "red", stroke = "black", stroke_width = 4)
+#' }
 #' @export
 RECT <- function(x, y = NULL, w, h, ...) {
     p <- as_coords(x, y)
@@ -27,26 +33,86 @@ RECT_HELPER <- function(x, y, w, h, ...) {
     MZ(x, y, ...)
 }
 
-#' Circle path convenience wrapper
+#' Ellipse path convenience wrapper
 #'
-#' `MAZ_CIRCLE()` is a wrapper around `M()` and `AZ()` to create
-#' circle shaped paths.
+#' `ELLIPSE()` is a wrapper around `M()` and `AZ()` to create
+#' ellipse shaped paths.
+#' `CIRCLE()` is a special case to create circle shaped paths.
 #' @inheritParams M
 #' @param r The radius of the circle.
+#' @param rx,ry The radii of the ellipse.
 #' @param ... Passed to [M()] and [AZ()].
 #' @return A [dee()] object.
 #' @examples
 #' CIRCLE(x = 5, y = 5, r = 2)
+#' ELLIPSE(x = 5, y = 5, rx = 2, ry = 3)
+#' if (requireNamespace("omsvg", quietly = TRUE) &&
+#'     requireNamespace("svgparser", quietly = TRUE)) {
+#'   plot(ELLIPSE(x = 5, y = 5, rx = 2, ry = 3),
+#'        height = 10, width = 10,
+#'        fill = "red", stroke = "black", stroke_width = 4)
+#' }
 #' @export
-CIRCLE <- function(x, y = NULL, r, ...) {
+ELLIPSE <- function(x, y = NULL, rx, ry = rx, ...) {
     p <- as_coords(x, y)
     x <- p$x
     y <- p$y
-    .mapply(CIRCLE_HELPER, list(x = x, y = y, r = r), list(...)) |>
+    .mapply(ELLIPSE_HELPER,
+            list(x = x, y = y, rx = rx, ry = ry),
+            list(...)) |>
         Reduce(`+.dee`, x = _)
 }
 
-CIRCLE_HELPER <- function(x, y, r, ...) {
-    M(x, y + r, ...) + 
-        AZ(r, r, 0, 0, 0, x, c(y - r, y + r), ...)
+#' @rdname ELLIPSE
+#' @export
+CIRCLE <- function(x, y = NULL, r, ...) {
+    ELLIPSE(x, y, r, r, ...)
+}
+
+ELLIPSE_HELPER <- function(x, y, rx, ry, ...) {
+    M(x, y + ry, ...) + 
+        AZ(rx, ry, 0, 0, 0, x, c(y - ry, y + ry), ...)
+}
+
+#' Polygon path convenience wrapper
+#'
+#' `POLYGON()` is a wrapper around `MZ()` to create
+#' polygon shaped paths.  
+#' If the argument `offset` is nonzero
+#' will use [polyclip::polyoffset()] to compute an offset region.
+#' @inheritParams M
+#' @param ... Passed to [MZ()].
+#' @param offset If a positive number the distance for *outward* polygon offsetting.  If a negative number the distance for *inward* polygon offsetting.
+#' @param linejoin If `offset` is nonzero the type of join operation
+#'                 to use at each vertex when computing the offset region.
+#' @param miterlimit Tolerance parameter if `offset` is nonzero
+#'                   and `linejoin = "miter"`.  See [polyclip::polyoffset()].
+#' @return A [dee()] object.
+#' @examples
+#' l <- list(x = c(2, 5, 8, 5), y = c(5, 8, 5, 2))
+#' po <- POLYGON(l, offset = 1)
+#' p <- POLYGON(l)
+#' pi <- POLYGON(l, offset = -1)
+#' if (requireNamespace("omsvg", quietly = TRUE) &&
+#'     requireNamespace("svgparser", quietly = TRUE)) {
+#'   plot(po + p + pi, height = 10, width = 10,
+#'        attrs = list(fill_rule = "evenodd"),
+#'        fill = "red", stroke = "black", stroke_width = 4)
+#' }
+#' @seealso [polyclip::polyoffset()] for details on computing the offset region when `offset` is non-zero.
+#' @export
+POLYGON <- function(x, y = NULL, ...,
+                    offset = 0,
+                    linejoin = c("miter", "round"),
+                    miterlimit = 4) {
+    if (offset == 0)
+        return (MZ(x, y, ...))
+    stopifnot(requireNamespace("polyclip", quietly = TRUE))
+    p <- as_coords(x, y)
+    x <- p$x
+    y <- p$y
+    jointype <- match.arg(linejoin)
+    xy <- polyclip::polyoffset(list(x = x, y = y), offset,
+                               jointype = jointype, miterlim = miterlimit)
+    MZ(xy, ...)
 }
--- tests/testthat/test-path.r
+++ tests/testthat/test-path.r
@@ -25,10 +25,27 @@ test_that("`M()`", {
                  "M 1 8")
 })
 
-test_that("`CIRCLE()`", {
+test_that("`ELLIPSE()`", {
     do.call(rlang::local_options, dee_options(default = TRUE))
     expect_true(CIRCLE(5, 5, 2) |> inherits("dee"))
     # plot(CIRCLE(5, 5, 4), height = 10, width = 10, fill = "red")
+    expect_true(ELLIPSE(5, 5, 2, 3) |> inherits("dee"))
+    # plot(ELLIPSE(5, 5, 2, 3), height = 10, width = 10, fill = "red")
+    # plot(ELLIPSE(5, 5, 3, 2), height = 10, width = 10, fill = "red")
+})
+
+test_that("`POLYGON()`", {
+    skip_if_not_installed("polyclip")
+    skip_on_cran()
+    do.call(rlang::local_options, 
+            dee_options(dee.digits = 0, default = TRUE))
+    l <- list(x = c(4, 4, 8, 8), y = c(4, 8, 8, 4))
+    expect_equal(POLYGON(l) |> format(),
+                 "M 4,4 4,8 8,8 8,4 Z")
+    expect_equal(POLYGON(l, offset = 1) |> format(),
+                 "M 9,9 3,9 3,3 9,3 Z")
+    expect_equal(POLYGON(l, offset = -1) |> format(),
+                 "M 7,7 5,7 5,5 7,5 Z")
 })
 
 test_that("`RECT()`", {
